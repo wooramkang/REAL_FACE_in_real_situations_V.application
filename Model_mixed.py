7d2a9c46dfe7e29340b025509cafaf79ab1130c1
WooramKang <omelisra@naver.com>
2018-08-29 12:29:36 +0900

    more than the best

--- Model_mixed.py
+++ Model_mixed.py
@@ -69,12 +69,13 @@ def conv2d_bn(X, nb_filter, num_row, num_col, padding='same', strides=(1, 1), us
     return X
 
 def inception_A(X):
+
     if K.image_data_format() == 'channels_first':
         channel_axis = 1
     else:
         channel_axis = -1
 
-    X1 = AveragePooling2D(pool_size=(3, 3), strides=(3, 3), data_format='channels_first')(X)
+    X1 = AveragePooling2D(pool_size=(3, 3), strides=(1,1), data_format='channels_first')(X)
     X1 = Conv2D(64, (1, 1), strides=(1, 1))(X1)
 
     X2 = Conv2D(96, (1, 1), strides=(1,1))(X)
@@ -91,12 +92,28 @@ def inception_A(X):
 
 
 def inception_B(X):
+
     if K.image_data_format() == 'channels_first':
         channel_axis = 1
     else:
         channel_axis = -1
 
+    X1 = AveragePooling2D(pool_size=(3, 3), strides=(1, 1), data_format='channels_first')(X)
+    X1 = Conv2D(128, (1, 1), strides=(1, 1))(X1)
 
+    X2 = Conv2D(384, (1, 1), strides=(1,1))(X)
+
+    X3 = Conv2D(192, (1, 1), strides=(1,1))(X)
+    X3 = Conv2D(224, (7, 1), strides=(1, 1))(X3)
+    X3 = Conv2D(256, (1, 7), strides=(1, 1))(X3)
+
+    X4 = Conv2D(192, (1, 1), strides=(1,1))(X)
+    X4 = Conv2D(192, (1, 7), strides=(1, 1))(X4)
+    X4 = Conv2D(224, (7, 1), strides=(1, 1))(X4)
+    X4 = Conv2D(224, (1, 7), strides=(1, 1))(X4)
+    X4 = Conv2D(256, (7, 1), strides=(1, 1))(X4)
+
+    X = concatenate([X1, X2, X3, X4], axis=channel_axis)
     return X
 
 
---
+++ working-log.md
@@ -0,0 +1,25 @@
+     to-do list
+     
+        1. for making embedding
+            inception-v4
+            triplet-loss
+            + etc 
+            => my work = Facenet - inception-v2 + inception-v4 -stn + stn else
+        
+        2. for denosing
+            denosing autoencoder
+            
+        3. for making resolution clear
+            super-resolution
+            especially, ESRCNN
+            
+        4. for removing lights
+            CLAHE
+            
+        5. for twisted face
+            affine transform
+                   
+                   
+     doing list
+     
+        1. Facenet - inception-v2 + inception-v4 (2018.08.28 ~)
